88c04968b28d3af19c82d182a926da93057bb400
DamoFD <voshall@yahoo.com>
2023-06-20 19:47:34 -0700

    menu item component

--- resources/js/Components/MenuItem.vue
+++ resources/js/Components/MenuItem.vue
@@ -1,6 +1,24 @@
 <template>
-    <div>
-
+    <div class="
+        w-full
+        lg:inline-block
+        hover:bg-gray-900
+        p-2
+        px-4
+        lg:pr-6
+        rounded-full
+        cursor-pointer
+        transition
+        duration-200
+        ease-in-out
+    "
+    >
+        <div class="flex items-center">
+            <component :is="icon" fillColor="#FFFFFF" :size="30" />
+            <span class="lg:block hidden text-white font-extrabold text-[23px] pl-4 mt-0.5">
+                {{ iconString }}
+            </span>
+        </div>
     </div>
 </template>
 
@@ -25,6 +43,7 @@ if (iconString.value === 'Explore') icon = Pound
 if (iconString.value === 'Notifications') icon = BellOutline
 if (iconString.value === 'Messages') icon = EmailOutline
 if (iconString.value === 'Profile') icon = AccountOutline
+
 </script>
 
 <style lang="scss" scoped>
